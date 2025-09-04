ef5a862461a1c1e862c7347d3928b66ffaf4e747
antwiroland <antwiroland58@gmail.com>
2025-09-04 18:14:42 +0000

    deploy to beantalk

--- src/main/java/com/ras/todo/controller/WelcomeController.java
+++ src/main/java/com/ras/todo/controller/WelcomeController.java
@@ -1,21 +1,21 @@
-package com.example.beanstalkapp.controller;
+// package com.example.beanstalkapp.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class WelcomeController {
+// @Controller
+// public class WelcomeController {
 
-    @GetMapping("/")
-    public String welcome(Model model) {
-        model.addAttribute("message", "Hello Welcome Beanstalk App");
-        model.addAttribute("title", "Beanstalk Application");
-        return "welcome";
-    }
+//     @GetMapping("/")
+//     public String welcome(Model model) {
+//         model.addAttribute("message", "Hello Welcome Beanstalk App");
+//         model.addAttribute("title", "Beanstalk Application");
+//         return "welcome";
+//     }
     
-    @GetMapping("/health")
-    public String healthCheck() {
-        return "OK";
-    }
-}
+//     @GetMapping("/health")
+//     public String healthCheck() {
+//         return "OK";
+//     }
+// }
